597c12ef2353afa6a8d68137aa168e1168aae44d
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-17 15:28:30 +0900

    検索機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   PER_PAGE = 20
 
   def index
-    @users = User.page(params[:page]).per(PER_PAGE)
+    @q = User.ransack(params[:q])
+    @users = @q.result.page(params[:page]).per(PER_PAGE)
   end
 end
